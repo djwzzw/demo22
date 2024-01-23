c4bba7ea76af5246d6e549f6c258e8c41dc91ee0
djwzzw <1424818476@qq.com>
2024-01-23 11:06:24 +0800

    Update TestController.java

--- demo2/src/main/java/com/example/demo2/Controller/TestController.java
+++ demo2/src/main/java/com/example/demo2/Controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @GetMapping("/helloword")
     public String helloWorld(){
-        String s = "hello worlsd";
+        String s = "hello world";
         return s;
     }
 }

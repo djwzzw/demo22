67b24dd9361ef8a997899ae5368e2aa5da627533
djwzzw <1424818476@qq.com>
2024-01-23 17:55:20 +0800

    commit new 1

--- demo2/src/main/java/com/example/demo2/Controller/TestController.java
+++ demo2/src/main/java/com/example/demo2/Controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo2.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @PostMapping("/helloword")
-    public String helloWorld(){
-        String s = "hello world";
-        System.out.println("jinlaile sss");
-        return s;
+    public ResponseEntity<String> helloWorld(String payload){
+        // 处理 Webhook 事件的逻辑
+        System.out.println("Received Webhook event:\n" + payload);
+
+        // 返回 JSON 响应
+        String jsonResponse = "{\"status\":\"Accepted\",\"message\":\"Webhook event successfully received.\"}";
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(jsonResponse);
     }
 }

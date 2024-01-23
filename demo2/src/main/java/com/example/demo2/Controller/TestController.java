9314046a756639c7e729b21da7dc9c02e2769c5f
djwzzw <1424818476@qq.com>
2024-01-23 18:33:02 +0800

    test new 2

--- demo2/src/main/java/com/example/demo2/Controller/TestController.java
+++ demo2/src/main/java/com/example/demo2/Controller/TestController.java
@@ -1,8 +1,10 @@
 package com.example.demo2.Controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @PostMapping("/helloword")
-    public ResponseEntity<String> helloWorld(String payload){
+    public ResponseEntity<String> helloWorld(@RequestBody JSONPObject pushEvent){
         // 处理 Webhook 事件的逻辑
-        System.out.println("Received Webhook event:\n" + payload);
+        // 提取有关 PR 的信息
+//        if (pushEvent. != null && !pushEvent.getCommits().isEmpty()) {
+//            pushEvent.getCommits().forEach(commit -> {
+//                System.out.println("Author: " + commit.getAuthor().getName());
+//                System.out.println("Comment: " + commit.getMessage());
+//                System.out.println("Files changed:");
+//                commit.getAdded().forEach(file -> System.out.println("- Added: " + file));
+//                commit.getModified().forEach(file -> System.out.println("- Modified: " + file));
+//                commit.getRemoved().forEach(file -> System.out.println("- Removed: " + file));
+//            });
+//        } else {
+//            System.out.println("No commits in push event.");
+//        }
 
         // 返回 JSON 响应
         String jsonResponse = "{\"status\":\"Accepted\",\"message\":\"Webhook event successfully received.\"}";
